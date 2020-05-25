9035a1e55797ca893c974fbcace65203d7c1712d
Usuario <Usuario@Usuario-PC>
2020-05-25 16:10:09 -0500

    tareas

---
+++ netbeans/src/netbeans/Ejercicios.java
@@ -0,0 +1,30 @@
+
+package netbeans;
+
+import java.util.Scanner;
+
+
+public class Ejercicios {
+  static Scanner entrada = new Scanner(System.in);
+    public void ejercicio1(){
+         
+
+        System.out.println("digite un numero");
+
+        int numero = entrada.nextInt();
+         if (numero % 2 == 0) {
+            System.out.println("es par");
+        } else {
+            System.out.println(" es impar");
+        }
+    }
+    public void ejercicio2(){
+        /*
+) Una compañía de seguros está abriendo un departamento de finanzas y estableció 
+        un programa para captar clientes, que consiste en lo siguiente: 
+        Si el monto por el que se efectúa la fianza es menor que $50.000, la cuota a pagar será el 3% 
+        del monto, y si el monto es mayor que $50.000, la cuota a pagar será el 2% del monto. 
+        La aseguradora desea determinar cuál será la cuota que debe pagar un cliente. */
+        
+    }
+}
