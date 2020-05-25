15e62dcbae7936aecd368214042f3302e7b96a18
Usuario <Usuario@Usuario-PC>
2020-05-25 14:02:09 -0500

    se validan los datos de login localmente

--- netbeans/src/netbeans/Netbeans.java
+++ netbeans/src/netbeans/Netbeans.java
@@ -15,7 +15,7 @@ public class Netbeans {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("20$eG,pjFf20");
     }
     
 }
